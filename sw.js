4249c33073e33dd8d4b8f608520c0f7bb6a0c24f
redme8180-cyber <redme8180@gmail.com>
2026-04-08 20:22:30 -0700

    Delete caching logic from service worker
    Remove service worker caching logic.

--- sw.js
+++ sw.js
@@ -1,10 +1,1 @@
-const CACHE_NAME = 'locpro-v3'; // On passe en v3 pour écraser les anciens tests
-const ASSETS = ['index.html', 'manifest.json'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
